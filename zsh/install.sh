9f867f9dc691a4cb98a1d45db887e6141aebdb22
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-08-07 23:20:14 -0600

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,2 @@
 sudo apt-get install curl zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-wget https://raw.githubusercontent.com/jeffpignataro/Bash-Tools/master/zsh/.zshrc
-mv .zshrc ~/.zshrc
