08582e866184f7d977e12efa766f6bfd5843f4a2
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-08-07 23:18:01 -0600

    Creating ohmyzsh installer

---
+++ zsh/install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install curl zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+wget https://raw.githubusercontent.com/jeffpignataro/Bash-Tools/master/zsh/.zshrc
+mv .zshrc ~/.zshrc
